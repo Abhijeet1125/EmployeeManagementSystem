cda0a9c1eb13356f0bddd5435fe941e2c426ce26
Abhijeet Kumar <abhijeet.iitp25@gmail.com>
2024-11-08 22:44:18 +0530

    v to V

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,16 +4,16 @@ import EmployeePage from "./employeePage/EmployeeLayout"
 import Test from "./Test";
 import EmpList from "./employeePage/EmpList";
 import AddEmployee from "./employeePage/AddEmployee";
-import EmployeeDetails from "./employeePage/viewEdit";
+import EmployeeDetails from "./employeePage/ViewEdit";
 import AttendanceDetails from "./employeePage/Attendance";
 import DepartmentPage from "./departmentPage/DepartmentLayout"
 import DeptList from "./departmentPage/DeptList"
 import AddDepartment from "./departmentPage/AddDepartment";
-import DepartmentDetails from "./departmentPage/viewEdit";
+import DepartmentDetails from "./departmentPage/ViewEdit";
 import DesignationPage from "./designationPage/DesignationLayout";
 import DesigList from "./designationPage/DesigList"
 import AddDesignation from "./designationPage/AddDesignation";
-import DesignationDetails from "./designationPage/viewEdit";
+import DesignationDetails from "./designationPage/ViewEdit";
 import SalaryDetails from "./employeePage/Salary"
 
 export { Layout, SignIn, 
